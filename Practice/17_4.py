86537bbabba8189ec498afc790bd83148e7490fb
Marco <marco_mireles@outlook.com>
2023-01-12 18:12:01 -0600

    17.4.3 skill drill 2

--- Practice/17_4.py
+++ Practice/17_4.py
@@ -1,4 +1,4 @@
-# Module 16.4.2 Spark DataFrames and Datasets
+# Module 17.4.2 Spark DataFrames and Datasets
 # Start Spark session
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("DataFrameBasics").getOrCreate()
@@ -80,3 +80,69 @@ dataframe.withColumn('doubleprice',dataframe['price']*2).show()
 dataframe.withColumn('add_one_dollar',dataframe['price']+1).show()
 # Half the price
 dataframe.withColumn('half_price',dataframe['price']/2).show()
+
+# 17.4.3 Spark Functions
+
+# Module 17.4.3 Spark Functions
+# Start Spark session
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("DataFrameFunctions").getOrCreate()
+     
+
+# Read in data from S3 Buckets
+from pyspark import SparkFiles
+url ="https://s3.amazonaws.com/dataviz-curriculum/day_1/wine.csv"
+spark.sparkContext.addFile(url)
+df = spark.read.csv(SparkFiles.get("wine.csv"), sep=",", header=True)
+
+# Show DataFrame
+df.show()
+     
+
+# Order a DataFrame by ascending values
+df.orderBy(df["points"].desc())
+     
+
+df.orderBy(df["points"].desc()).show(5)
+     
+
+#Skill Drill 17.4.3
+df.orderBy(df["points"].asc()).show(50)
+     
+
+# Import Functions
+from pyspark.sql.functions import avg 
+df.select(avg("points")).show()
+     
+
+# Filter SQL method
+df.filter("price<20").show(5)
+     
+
+# Filter Python method
+df.filter("price<20").show(5)
+# Filter by price on certain columns
+df.filter("price<20").select(['points','country', 'winery','price']).show(5)
+
+# Filter on exact state
+df.filter(df["country"] == "US").show()
+     
+
+# Skill Drill 17.4.3 1/2
+# Filter Python method
+df.filter("price>15").show(5)
+# Filter by price on certain columns
+df.filter("price>15").select(['points','country', 'winery','price']).show(5)
+
+# Filter on exact state
+df.filter(df["province"] == "California").show()
+     
+
+# Skill Drill 17.4.3 2/2
+# Filter Python method
+df.filter("price>15").show(5)
+# Filter by price on certain columns
+df.filter("price>15").select(['points','country', 'winery','price']).show(5)
+
+# Filter on exact state
+df.filter(df["province"] == "California").show()
