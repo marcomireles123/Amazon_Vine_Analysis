d731ec68f2fb682ba6439393ebae39919174e574
Marco <marco_mireles@outlook.com>
2023-01-13 23:29:28 -0600

    17.6.2 notes

---
+++ Practice/17_6_2.py
@@ -0,0 +1,23 @@
+import os
+# Find the latest version of spark 3.0  from  http://www.apache.org/dist/spark/ and enter as the spark version
+# For example:
+# spark_version = 'spark-3.0.3'
+spark_version = 'spark-3.3.1'
+os.environ['SPARK_VERSION'] = spark_version# Install Spark and Java
+!apt-get update
+!apt-get install openjdk-11-jdk-headless -qq > /dev/null
+!wget -q  http://www.apache.org/dist/spark/$SPARK_VERSION/$SPARK_VERSION-bin-hadoop3.tgz
+!tar xf $SPARK_VERSION-bin-hadoop3.tgz
+!pip install -q findspark
+os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-11-openjdk-amd64"
+os.environ["SPARK_HOME"] = f"/content/{spark_version}-bin-hadoop3"  # Start a SparkSession
+
+import findspark
+
+findspark.init()
+
+# Start Spark session
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("Tokens").getOrCreate()
+
+from pyspark.ml.feature import Tokenizer
